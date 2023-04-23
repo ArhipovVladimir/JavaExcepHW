60306246a1e573b71427ce5e90228a35f2b8c8bb
ArhipovVS <arhipof05@yndex.ru>
2023-04-24 02:28:53 +0700

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+//Реализуйте метод checkArray(Integer[] arr), принимающий в качестве аргумента целочисленный
+// одномерный массив.
+// Метод должен пройтись по каждому элементу и проверить что он не равен null.
+//А теперь реализуйте следующую логику:
+//Если в какой-то ячейке встретился null, то необходимо “оповестить” об этом пользователя
+//Если null’ы встретились в нескольких ячейках, то идеально было бы все их “подсветить”
+
 
 public class Main {
+
     public static void main(String[] args) {
+        Integer [] array = {1,0,0,null,1,null,0,1};
+
+        int res = checkArray(array);
+        try {
+            if (res > 0) {
+                throw new RuntimeException("Имеет элемент null");
+            }
+        } finally {
+            System.out.printf("элементов null %d", res);
+            System.out.println();
+        }
+
 
     }
+
+    public static Integer checkArray(Integer[] arr){
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==null){
+                count++;
+
+            }
+        }
+        return count;
+    }
+
+
 }
